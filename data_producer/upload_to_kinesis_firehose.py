14219fc4fa44d215ad38b0edf7dd80bb8e362a34
William Librata <william.librata@gmail.com>
2020-09-01 22:19:45 +1000

    Add code to upload to firehose

--- data_producer/upload_to_kinesis_firehose.py
+++ data_producer/upload_to_kinesis_firehose.py
@@ -30,7 +30,7 @@ def unsubscribe(pubnub, listener, channel_name):
     print('unsubscribed')
 
 
-def upload_data(channel_name, subscriber_key, stream_name, partition_key, data_limit=10):
+def upload_data(channel_name, subscriber_key, stream_name, data_limit=10):
     # setup data source
     pubnub = setup_pubnub(subscriber_key)
     listener = setup_listener(pubnub, channel_name)
@@ -39,17 +39,20 @@ def upload_data(channel_name, subscriber_key, stream_name, partition_key, data_l
     counter = 1
 
     # setup boto
-    client = boto3.client('kinesis')
+    client = boto3.client('firehose')
 
     # loop
     while(counter < data_limit):
         # send to kinesis stream
         data = listener.wait_for_message_on(channel_name).message
         print(data)
+
+        record = {
+            'Data': json.dumps(data)
+        }
         response = client.put_record(
-            StreamName=stream_name,
-            Data=json.dumps(data),
-            PartitionKey=partition_key,
+            DeliveryStreamName=stream_name,
+            Record=record
         )
         print(response)
         counter += 1
@@ -57,7 +60,7 @@ def upload_data(channel_name, subscriber_key, stream_name, partition_key, data_l
     unsubscribe(pubnub, listener, channel_name)
 
 
-def upload_data_batch(channel_name, subscriber_key, stream_name, partition_key, batch_size=10, data_limit=10):
+def upload_data_batch(channel_name, subscriber_key, stream_name, batch_size=10, data_limit=10):
     # setup data source
     pubnub = setup_pubnub(subscriber_key)
     listener = setup_listener(pubnub, channel_name)
@@ -66,7 +69,7 @@ def upload_data_batch(channel_name, subscriber_key, stream_name, partition_key, 
     counter = 1
 
     # setup boto
-    client = boto3.client('kinesis')
+    client = boto3.client('firehose')
 
     # set records to be list
     records = []
@@ -77,17 +80,16 @@ def upload_data_batch(channel_name, subscriber_key, stream_name, partition_key, 
         # send to kinesis stream
         # check if the records is at batch size
         if len(records) == batch_size:
-            response = client.put_records(
+            response = client.put_record_batch(
+                DeliveryStreamName=stream_name,
                 Records=records,
-                StreamName=stream_name
             )
             print(response)
             # once uploaded, clear records list
             records = []
 
         record = {
-                    'Data': json.dumps(listener.wait_for_message_on(channel_name).message),
-                    'PartitionKey': partition_key
+                    'Data': json.dumps(listener.wait_for_message_on(channel_name).message)
                  }
         # add record to the list
         records.append(record)
@@ -101,13 +103,13 @@ if __name__ == '__main__':
     # setup parameter
     channel_name = 'pubnub-sensor-network'
     subscriber_key = 'sub-c-5f1b7c8e-fbee-11e3-aa40-02ee2ddab7fe'
-    stream_name = 'DataStream'
+    stream_name = 'sensor-network-stream'
     partition_key = 'HADES-SensorNetworkData'
-    data_limit = 20
+    data_limit = 2000
     batch_size = 10
 
     # upload data one at a time
-    #upload_data(channel_name, subscriber_key, stream_name, partition_key, data_limit)
+    upload_data(channel_name, subscriber_key, stream_name, data_limit)
 
     # upload data in batch
-    upload_data_batch(channel_name, subscriber_key, stream_name, partition_key, batch_size, data_limit)
+    #upload_data_batch(channel_name, subscriber_key, stream_name, batch_size, data_limit)
